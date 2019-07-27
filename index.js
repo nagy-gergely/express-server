3b73ca03708d4c4fa021793924726ac75b73d09f
nagy-gergely <nagy.gergely.9211@gmail.com>
2019-07-27 17:05:06 +0200

    add some configuration

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const authRoute = require('./api/auth');
+const errorHandler = require('./handler/errorHandler');
+const { loginRequired } = require('./middleware/auth');
+
+const app = express();
+
+dotenv.config();
+mongoose.connect(process.env.DB_CONNECTION_STRING, { useNewUrlParser: true }, () => {
+    console.log('Database connected.');
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+app.use(express.json());
+app.use('/auth', authRoute);
+
+app.get('/', loginRequired, (req, res, next) => {
+    console.log("decoded");
+})
+
+app.get('*', (req, res, next) => {
+    let err = new Error('404 Page Not Found');
+    next(err);
+});
+
+app.use(errorHandler);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server started.');
+});
